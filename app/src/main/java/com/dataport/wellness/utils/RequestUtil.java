3a36c412e5be925f5e002106bd0c4ff7fbd28c02
阎文成 <yanwc@dataport.com.cn>
2023-05-16 14:12:54 +0800

    粗封装request

--- app/src/main/java/com/dataport/wellness/utils/RequestUtil.java
+++ app/src/main/java/com/dataport/wellness/utils/RequestUtil.java
@@ -1,9 +1,12 @@
 package com.dataport.wellness.utils;
 
+import android.widget.Toast;
+
 import java.util.Map;
 
 public class RequestUtil {
     public static void get(String url, Map<String,Object> formMap){
         System.out.println();
+        Toast.makeText(ContextUtil.getContext(),"测试",Toast.LENGTH_LONG).show();
     }
 }
