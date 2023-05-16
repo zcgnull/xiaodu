8769a39d9bc17fe46791ed39e875bac8dbfb4ab6
阎文成 <yanwc@dataport.com.cn>
2023-05-16 14:04:05 +0800

    粗封装request

--- app/src/main/java/com/dataport/wellness/api/WellNessApi.java
+++ app/src/main/java/com/dataport/wellness/api/WellNessApi.java
@@ -1,6 +1,11 @@
 package com.dataport.wellness.api;
 
 
+import android.content.Context;
+
+import com.dataport.wellness.R;
+import com.dataport.wellness.utils.RequestUtil;
+
 import org.json.JSONObject;
 
 import java.net.URLEncoder;
@@ -9,12 +14,13 @@ import java.util.Map;
 
 /*康养管家后台接口*/
 public class WellNessApi {
+    private static Context mContext;
     //获取设备sn
     public static JSONObject getDeviceSn(String deviceId,String apiAccessToken){
         Map<String,Object>mp=new HashMap<>();
         mp.put("deviceId",deviceId);
         mp.put("apiAccessToken", URLEncoder.encode(apiAccessToken));
-//        RequestUtil.get("http://10.20.1.13:48080/admin-api/dueros/device-warehouse/get-device-sn", mp);
+        RequestUtil.get(mContext.getResources().getString(R.string.well_ness_api_url)+"/admin-api/dueros/device-warehouse/get-device-sn", mp);
         return null;
     }
 }
---
+++ app/src/main/java/com/dataport/wellness/utils/RequestUtil.java
@@ -0,0 +1,9 @@
+package com.dataport.wellness.utils;
+
+import java.util.Map;
+
+public class RequestUtil {
+    public static void get(String url, Map<String,Object> formMap){
+        System.out.println();
+    }
+}
