ead6688df5e4827c6f8e7907c6e9f97144f82260
阎文成 <yanwc@dataport.com.cn>
2023-05-22 11:41:12 +0800

    okhttp 忽略ssl证书

--- app/src/main/java/com/dataport/wellness/application/BotsdkApplication.java
+++ app/src/main/java/com/dataport/wellness/application/BotsdkApplication.java
@@ -11,6 +11,7 @@ import com.dataport.wellness.botsdk.BotMessageListener;
 import com.dataport.wellness.botsdk.BotSDKUtils;
 import com.dataport.wellness.http.RequestHandler;
 import com.dataport.wellness.http.RequestServer;
+import com.dataport.wellness.http.SSLSocketClient;
 import com.dataport.wellness.utils.BotConstants;
 import com.dataport.wellness.utils.ContextUtil;
 import com.hjq.http.EasyConfig;
@@ -63,6 +64,8 @@ public class BotsdkApplication extends Application {
         registerActivityLifecycleCallbacks(new ActivityContollor());
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .sslSocketFactory(SSLSocketClient.getSSLSocketFactory(), SSLSocketClient.getX509TrustManager())
+                .hostnameVerifier(SSLSocketClient.getHostnameVerifier())
                 .build();
 
         EasyConfig.with(okHttpClient)
